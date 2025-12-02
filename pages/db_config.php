bd2f525e2b5caeb8d36123e16f31fa9ec238f06f
Zidane <75941907+Zidane404@users.noreply.github.com>
2025-12-02 22:07:14 +0000

    Updated item_page with Nav Bar, Added Functional Cart Page.

---
+++ pages/db_config.php
@@ -0,0 +1,31 @@
+<?php
+
+$useLocal = true;
+
+if ($useLocal) {
+    // FOR LOCAL MYSQL TESTING
+    $db_host = '127.0.0.1';
+    $db_name = 'cs2team8_db04';
+    $db_user = 'root';
+    $db_pass = '';
+} else {
+    // FOR LIVE SERVER
+    $db_host = 'localhost';
+    $db_name = 'cs2team8_db04';
+    $db_user = 'cs2team8';
+    $db_pass = 'F3lCvksLmJqDqmsyllNrjsF8R';
+}
+
+$dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $db_user, $db_pass, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode(['error' => 'Database connection failed']);
+    exit;
+}
+
